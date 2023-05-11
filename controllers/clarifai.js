8615048d7c291702141ed736f3c654895f3109b2
todayisvin <kevinpratamaoke@gmail.com>
2023-05-11 19:36:36 +0700

    fix: change model id to general image detection

--- controllers/clarifai.js
+++ controllers/clarifai.js
@@ -15,7 +15,7 @@ exports.detector = (req, res) => {
         "user_id": "clarifai",
         "app_id": "main"
       },
-      model_id: "food-item-recognition",
+      model_id: "general-image-detection",
       // version_id: "1d5fd481e0cf4826aa72ec3ff049e044",  // This is optional. Defaults to the latest model version
       inputs: [
         { data: { image: { base64: imageBytes } } }
@@ -32,13 +32,16 @@ exports.detector = (req, res) => {
       }
 
       const output = response.outputs[0];
+      const ingredients = [];
 
       console.log("Predicted concepts:");
-      for (const concept of output.data.concepts.sort()) {
-        console.log(concept.name + " " + concept.value);
+      // console.log(output.data.regions);
+      for (const region of output.data.regions) {
+        ingredients.push(region.data.concepts[0].name);
+        console.log(region.data.concepts[0].name);
       }
 
-      axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=3798ef43760f4a10982037daf9a35c40&ingredients=${output.data.concepts[0].name}`)
+      axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=3798ef43760f4a10982037daf9a35c40&ingredients=${ingredients.join(',')}`)
         .then((spoonacularResponse) => {
           const data = spoonacularResponse.data;
           console.log("response Spoon", spoonacularResponse.data)
